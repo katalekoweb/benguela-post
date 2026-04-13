7902f09d12b2cd0a746fd034cecfa17dd70c7bd8
Kataleko <katalekoweb@gmail.com>
2026-04-13 19:33:30 +0100

    feat: create data mock

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const posts = [
+    {
+        title: "Noticia 1",
+        text: "Lorem ipsum teste teste teste",
+        banner: "image.png",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Noticia 2",
+        text: "Lorem ipsum teste teste teste",
+        banner: "image.png",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Noticia 3",
+        text: "Lorem ipsum teste teste teste",
+        banner: "image.png",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Noticia 4",
+        text: "Lorem ipsum teste teste teste",
+        banner: "image.png",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Noticia 5",
+        text: "Lorem ipsum teste teste teste",
+        banner: "image.png",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Noticia 6",
+        text: "Lorem ipsum teste teste teste",
+        banner: "image.png",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Noticia 7",
+        text: "Lorem ipsum teste teste teste",
+        banner: "image.png",
+        likes: 2,
+        comments: 4
+    }
+]
